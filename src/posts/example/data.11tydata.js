8344c34b44dc9e9e9c1d40cbc2a0207ae09d70cd
aironou <github@aironou.com.br>
2026-06-30 02:19:04 -0300

    feat: create example post

---
+++ src/posts/example/data.11tydata.js
@@ -0,0 +1,5 @@
+export default {
+    stylesheet: '../assets/example.min.css',
+    script: '../assets/example.min.js',
+    banner: '../assets/example.png',
+}
---
+++ src/posts/example/la.md
@@ -0,0 +1,6 @@
+---
+title: titulus
+subtitle: subtitulus
+---
+
+exemplum
