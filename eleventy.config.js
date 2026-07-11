ad371ae6fee491ac9b6f3d1547d05eecc3510b01
aironou <github@aironou.com.br>
2026-07-10 22:59:12 -0300

    feat: config mermaid-it plugins

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,3 +1,7 @@
+import markdownIt from "markdown-it";
+import anchor from 'markdown-it-anchor';
+import footnote_plugin from "markdown-it-footnote";
+
 export default function (config) {
     config.setInputDirectory('src/posts');
     config.setIncludesDirectory('.');
@@ -9,4 +13,11 @@ export default function (config) {
 
     config.setDataFileBaseName('data');
     config.setDataFileSuffixes(['.11tydata']);
+
+    config.setLibrary(
+        'md',
+        markdownIt({html: true, breaks: true, linkify: true})
+            .use(anchor, {permalink: anchor.permalink['headerLink']()})
+            .use(footnote_plugin)
+    )
 };
